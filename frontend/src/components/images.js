51cf383e667a9a91b5af86de70b9e2c13a119a09
Gianni Wiemers <g.wiemers@student.tudelft.nl>
2021-05-26 13:03:26 +0200

    Give images 1:1 aspect ratio

--- frontend/src/components/images.js
+++ frontend/src/components/images.js
@@ -9,7 +9,7 @@ const imageList = [
 const images = () => {
     var imgs = []
     for(var i = 0; i < 20; i++) {
-        imgs.push(<div className="ImageContainer"><img src={imageList[0].url}/></div>)
+        imgs.push(<div className="ImageContainer" style={{backgroundImage: 'url(' + imageList[0].url + ')'}}></div>)
     }
     return (
         <div className="Images Centering">
